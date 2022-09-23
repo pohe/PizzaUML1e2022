d227121ed3f9f47328ee41292749cde7872053c8
Poul Henriksen <pohe@easj.dk>
2022-09-23 12:39:34 +0200

    Add project files.

---
+++ PizzaUML1e2022/Pizza.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaUML1e2022
+{
+    public class Pizza
+    {
+
+		private int _pno;
+        private string _pizzaName;
+        private double _price;
+		private string _ingredients;
+
+		public string Ingredients
+		{
+			get { return _ingredients; }
+			set { _ingredients = value; }
+		}
+
+		public int PNO
+		{
+			get { return _pno; }
+			set { _pno = value; }
+		}
+
+
+
+		public string PizzaName
+		{
+			get { return _pizzaName; }
+			set { _pizzaName = value; }
+		}
+
+
+		
+
+		public double Price
+		{
+			get { return _price; }
+			set { _price = value; }
+		}
+
+		public Pizza(int pno, double price, string name, string  ingredients)
+		{
+			_pno = pno;
+			_pizzaName = name;
+			_price = price;
+			_ingredients = ingredients;
+		}
+
+		public override string ToString()
+		{
+			return $"Pizza nummer { _pno} navn { _pizzaName} ingredienser {_ingredients} pris {_price}";
+		}
+
+	}
+}
---
+++ PizzaUML1e2022/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PizzaUML1e2022;
+
+Console.WriteLine("Hello, World!");
+
+
+Pizza p1 = new Pizza(1, 75.5, "Magaritha", "Tomat, ost");
+Pizza p2 = new Pizza(2, 85.5, "Hawaii", "Tomat, ost, anannas, skinke");
+
+Console.WriteLine(p2.ToString());
+
+
+
+Console.ReadLine();
